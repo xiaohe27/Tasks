a2a5e900c95999354277d91b529e04692f87b9b2
xiaohe27 <xiaoguoyi27@gmail.com>
2015-08-26 20:39:22 -0500

    modify enforce test

--- EnforceTest/Main.java
+++ EnforceTest/Main.java
@@ -1,10 +1,4 @@
-class SimpleRunnable1 implements Runnable{
-    public void run(){
-        System.out.println("Thread " + Thread.currentThread().getName());
-    }
-}
-
-class SimpleRunnable2 implements Runnable{
+class SimpleRunnable implements Runnable{
     public void run(){
         System.out.println("Thread " + Thread.currentThread().getName());
     }
@@ -12,14 +6,11 @@ class SimpleRunnable2 implements Runnable{
 
 public class Main{
     public static void main(String args[]) throws Exception{
-	System.out.println("begin");
-        Thread t1 = new Thread (new SimpleRunnable1());
-        Thread t2 = new Thread (new SimpleRunnable2());
+        Thread t1 = new Thread (new SimpleRunnable());
+        Thread t2 = new Thread (new SimpleRunnable());
         t1.setName("T1");
         t2.setName("T2");
-System.out.println("before t1");
         t1.start();
-System.out.println("before t2");
         t2.start();
         t1.join();
         t2.join();
