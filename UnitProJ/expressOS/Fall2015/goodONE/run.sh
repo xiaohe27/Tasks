f312551d58f57fa97b4c2196a6d2d0e419417ee2
xiaohe27 <xiaoguoyi27@gmail.com>
2015-09-07 18:11:18 -0500

    update

--- UnitProJ/expressOS/Fall2015/goodONE/run.sh
+++ UnitProJ/expressOS/Fall2015/goodONE/run.sh
@@ -7,7 +7,7 @@ echo "Start time : $Start"
 Dafny.exe $1
 
 End=$(date +"%s")
-echo "\nFinish time : $End"
+echo "Finish time : $End"
 Diff=$(( $End - $Start ))
-echo "\nIt took Dafny $Diff seconds to verify the program!"
+echo "It took Dafny $Diff seconds to verify the program!"
 
