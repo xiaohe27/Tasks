d1cf5b654814733a32e0bf8bb14c2cfd0afe1eed
xiaohe27 <xiaoguoyi27@gmail.com>
2015-10-08 14:51:20 -0500

    save

--- InterestingLogAndProperty/PerformanceCompare/complex/300M-Log-delete-1-2.sh
+++ InterestingLogAndProperty/PerformanceCompare/complex/300M-Log-delete-1-2.sh
@@ -5,9 +5,10 @@ echo "Start time : $Start" > $Out
 #/home/xiaohe/SW/offline-log-analysis/30MB-Log/0.5M.log
 #/home/xiaohe/SW/offline-log-analysis/30MB-Log/oneM.log
 #/home/xiaohe/SW/offline-log-analysis/ldcc4Monpoly
-#/home/xiaohe/SW/offline-log-analysis/30MB-Log/oneM-fault.log
+#/home/xiaohe/SW/offline-log-analysis/30MB-Log/oneM-fault-end.log
+#/home/xiaohe/SW/offline-log-analysis/30MB-Log/oneM-fault-begin.log
 
-/home/xiaohe/SW/offline-log-analysis/existingApp/monpoly-1.1.2/monpoly -sig delete.sig -formula delete12.mfotl -negate -log /home/xiaohe/SW/offline-log-analysis/30MB-Log/oneM-fault.log > violations.txt
+/home/xiaohe/SW/offline-log-analysis/existingApp/monpoly-1.1.2/monpoly -sig delete.sig -formula delete12.mfotl -negate -log /home/xiaohe/SW/offline-log-analysis/30MB-Log/oneM-fault-begin.log > violations.txt
 
 End=$(date +"%s")
 echo "Finish time : $End" >> $Out
