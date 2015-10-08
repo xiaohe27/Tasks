efefcbb0cd17ea546da6ab796c0692ca689e70c6
xiaohe27 <xiaoguoyi27@gmail.com>
2015-10-08 14:43:49 -0500

    add new log file

--- InterestingLogAndProperty/PerformanceCompare/complex/300M-Log-delete-1-2.sh
+++ InterestingLogAndProperty/PerformanceCompare/complex/300M-Log-delete-1-2.sh
@@ -5,10 +5,11 @@ echo "Start time : $Start" > $Out
 #/home/xiaohe/SW/offline-log-analysis/30MB-Log/0.5M.log
 #/home/xiaohe/SW/offline-log-analysis/30MB-Log/oneM.log
 #/home/xiaohe/SW/offline-log-analysis/ldcc4Monpoly
+#/home/xiaohe/SW/offline-log-analysis/30MB-Log/oneM-fault.log
 
-/home/xiaohe/SW/offline-log-analysis/existingApp/monpoly-1.1.2/monpoly -sig delete.sig -formula delete12.mfotl -negate -log /home/xiaohe/SW/offline-log-analysis/30MB-Log/oneM.log > violations.txt
+/home/xiaohe/SW/offline-log-analysis/existingApp/monpoly-1.1.2/monpoly -sig delete.sig -formula delete12.mfotl -negate -log /home/xiaohe/SW/offline-log-analysis/30MB-Log/oneM-fault.log > violations.txt
 
 End=$(date +"%s")
 echo "Finish time : $End" >> $Out
 Diff=$(( $End - $Start ))
-echo "\nIt took Monpoly (opt) $Diff seconds to output all violations of delete-1-2 Property in the 300MB log file!" >> $Out
+echo "\nIt took Monpoly (opt) $Diff seconds to output all violations of delete-1-2 Property in the 33.5MB log file!" >> $Out
