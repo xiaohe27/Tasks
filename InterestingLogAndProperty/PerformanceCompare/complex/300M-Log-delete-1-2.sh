8923785d44ede212b03f85adf6904fecb13e832f
xiaohe27 <xiaoguoyi27@gmail.com>
2015-10-08 14:12:32 -0500

    add

---
+++ InterestingLogAndProperty/PerformanceCompare/complex/300M-Log-delete-1-2.sh
@@ -0,0 +1,14 @@
+Out=timeFor9MLog-Mon-Siebel.txt
+Start=$(date +"%s")
+echo "Start time : $Start" > $Out
+
+#/home/xiaohe/SW/offline-log-analysis/30MB-Log/0.5M.log
+#/home/xiaohe/SW/offline-log-analysis/30MB-Log/oneM.log
+#/home/xiaohe/SW/offline-log-analysis/ldcc4Monpoly
+
+/home/xiaohe/SW/offline-log-analysis/existingApp/monpoly-1.1.2/monpoly -sig delete.sig -formula delete12.mfotl -negate -log /home/xiaohe/SW/offline-log-analysis/30MB-Log/oneM.log > violations.txt
+
+End=$(date +"%s")
+echo "Finish time : $End" >> $Out
+Diff=$(( $End - $Start ))
+echo "\nIt took Monpoly (opt) $Diff seconds to output all violations of delete-1-2 Property in the 300MB log file!" >> $Out
