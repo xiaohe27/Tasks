e1c223b8b7173cdc8e0dcfc5c0d4535421285f22
xiaohe27 <xiaoguoyi27@gmail.com>
2015-10-12 16:51:36 -0500

    add

--- InterestingLogAndProperty/PerformanceCompare/complex/300M-Log-delete-1-2.sh
+++ InterestingLogAndProperty/PerformanceCompare/complex/300M-Log-delete-1-2.sh
@@ -8,7 +8,8 @@ echo "Start time : $Start" > $Out
 #/home/xiaohe/SW/offline-log-analysis/30MB-Log/oneM-fault-end.log
 #/home/xiaohe/SW/offline-log-analysis/30MB-Log/oneM-fault-begin.log
 
-FORMULA=delete12-simplified.mfotl
+#FORMULA=delete12-simplified.mfotl
+FORMULA=delete12.mfotl
 
 /home/xiaohe/SW/offline-log-analysis/existingApp/monpoly-1.1.2/monpoly -sig delete.sig -formula $FORMULA -negate -log /home/xiaohe/SW/offline-log-analysis/ldcc4Monpoly 
 
