6be09433d76874ccea852c46f7a5e377e8b1b74e
xiaohe27 <xiaoguoyi27@gmail.com>
2015-10-26 10:41:44 -0500

    add performance compare

--- InterestingLogAndProperty/PerformanceCompare/complex/rvm-spec/good/run7G.sh
+++ InterestingLogAndProperty/PerformanceCompare/complex/rvm-spec/good/run7G.sh
@@ -6,9 +6,9 @@ echo "Start time : $Start" > $Out
 #108000 is 30h
 #/home/xiaohe/SW/offline-log-analysis/ldcc4Monpoly
 #/home/xiaohe/workspace/DATA/ldccComplete_MonpolyStyle
-java rvm.LogReader /home/xiaohe/SW/offline-log-analysis/ldcc4Monpoly 2> violation.txt
+java rvm.LogReader /home/xiaohe/workspace/DATA/ldccComplete_MonpolyStyle 2> violation.txt
 
 End=$(date +"%s")
 echo "\nFinish time : $End" >> $Out
 Diff=$(( $End - $Start ))
-echo "\nIt took RV-Log $Diff seconds to monitor del12 property in the 9M log ldcc4Monpoly at siebel" >> $Out
+echo "\nIt took RV-Log $Diff seconds to monitor del12 property in the 7.6G complete Nokia log at siebel" >> $Out
