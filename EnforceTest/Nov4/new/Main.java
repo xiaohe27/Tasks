35ca7c3f17a4a53b4b2f32908064bb5dd146ac20
xiaohe27 <xiaoguoyi27@gmail.com>
2015-11-04 22:32:59 -0600

    add

---
+++ EnforceTest/Nov4/new/Main.java
@@ -0,0 +1,27 @@
+class SimpleRunnable1 implements Runnable{
+    public void run(){
+        System.out.println("Thread " + Thread.currentThread().getName());
+    }
+}
+
+class SimpleRunnable2 implements Runnable{
+    public void run(){
+        System.out.println("Thread " + Thread.currentThread().getName());
+    }
+}
+
+public class Main{
+    public static void main(String args[]) throws Exception{
+	System.out.println("begin");
+        Thread t1 = new Thread (new SimpleRunnable1());
+        Thread t2 = new Thread (new SimpleRunnable2());
+        t1.setName("T1");
+        t2.setName("T2");
+System.out.println("before t1");
+        t1.start();
+System.out.println("before t2");
+        t2.start();
+        t1.join();
+        t2.join();
+    }
+}
